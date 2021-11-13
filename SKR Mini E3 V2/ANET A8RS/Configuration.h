b0aeb9e3efd225654a3750b5f4bf2a6b2a451d5e
David <davibloch@googlemail.com>
2021-11-13 13:45:53 +0100

    use fan controller

--- SKR Mini E3 V2/ANET A8RS/Configuration.h
+++ SKR Mini E3 V2/ANET A8RS/Configuration.h
@@ -1195,7 +1195,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 30, -15, -3.5 }
+#define NOZZLE_TO_PROBE_OFFSET { 30, -15, -3.3 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
